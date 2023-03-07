cc18130c89c1126f7051565fbb1715961ca3832e
mteam88 <matthew8edelen@gmail.com>
2023-03-07 15:37:35 -0500

    refactor so digest runs on a transaction

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-use std::ops::Deref;
-
-use ethers::{providers::{Middleware, Provider}, types::{Block, Transaction}};
+use ethers::{
+    providers::{Middleware, Provider},
+    types::Transaction,
+};
 use ethers_providers::{Http, ProviderExt, StreamExt};
 use eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let provider =
-        Provider::<Http>::connect("https://ethereum.publicnode.com")
-            .await;
+    let provider = Provider::<Http>::connect("https://ethereum.publicnode.com").await;
     let mut stream = provider.watch_blocks().await?;
     // call digest on every block in stream
     while let Some(block) = stream.next().await {
-        let block = provider.get_block_with_txs(block).await?;
-        digest(block.unwrap()).await?;
+        let block = provider.get_block_with_txs(block).await?.unwrap();
+        block.transactions.iter().for_each(|tx| {
+            tokio::spawn(digest(tx.clone()));
+        });
     }
 
     Ok(())
 }
 
-async fn digest(block: Block<Transaction>) -> Result<()> {
+async fn digest(tx: Transaction) -> Result<()> {
     // do something with block
-    block.transactions.iter().for_each(|tx| {
-        println!("{:?} --> {:?}", tx.hash, String::from_utf8_lossy(&tx.deref().input.to_vec()));
-    });
+    println!(
+        "{:?} --> {:?}",
+        tx.hash,
+        String::from_utf8_lossy(&tx.input.to_vec())
+    );
     Ok(())
 }
